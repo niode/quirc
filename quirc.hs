3c143b8a210d145db4a71d4c328ed9896ea76f9d
Nathan Harms <nathanharms@gmail.com>
2015-02-13 16:42:55 -0700

    Restarted again.

--- quirc.hs
+++ quirc.hs
@@ -1,24 +1,10 @@
 data Ket = Ket Int Int
          | Tens Int [Ket]
          | Sum Int [Ket]
-instance Show Ket where
-  show (Ket a b) = (show a) ++ "|" ++ (show b) ++ ">"
-  show (Tens a b) = (show a) ++ (show b)
-  show (Sum a b) = (show a) ++ (show b)
+    deriving Show
 
-foldKet fk ft fs k = case k of
-  Ket a b -> fk a b
-  Tens a b -> ft a b
-  Sum a b -> fs a b
-
-h = foldKet (\a b -> if b == 0 then (Sum 1 [Ket 1 0, Ket 1 1])
-                               else (Sum 1 [Ket 1 0, Ket (-1) 1]))
-            (\a b -> Tens a b)
-            (\a b -> Sum a b)
-x = foldKet (\a b -> if b == 0 then Ket a 1 else Ket a 0)
-            (\a b -> Tens a b)
-            (\a b -> Sum a b)
-
-z = foldKet (\a b -> if b == 0 then Ket a b else Ket (-a) b)
-            (\a b -> Tens a b)
-            (\a b -> Sum a b)
+h::Ket->Ket
+h (Ket a 0) = Sum a [Ket 1 0, Ket 1 1]
+h (Ket a 1) = Sum a [Ket 1 0, Ket (-1) 1]
+h (Sum a k) = Sum a (map h k)
+h (Tens a (k:ks)) = Tens a (h k : ks)
