208f8be4d871a84503861d61dbc5585a1f3db8c2
Nathan <nathanharms@gmail.com>
2015-02-13 15:48:57 -0700

    Modified a lot.

--- quirc.hs
+++ quirc.hs
@@ -1,12 +1,24 @@
-import Numeric.LinearAlgebra
+data Ket = Ket Int Int
+         | Tens Int [Ket]
+         | Sum Int [Ket]
+instance Show Ket where
+  show (Ket a b) = (show a) ++ "|" ++ (show b) ++ ">"
+  show (Tens a b) = (show a) ++ (show b)
+  show (Sum a b) = (show a) ++ (show b)
 
-data Ket a = Ket (Vector a)
-           | KTensor (Ket a) (Ket a)
+foldKet fk ft fs k = case k of
+  Ket a b -> fk a b
+  Tens a b -> ft a b
+  Sum a b -> fs a b
 
-data Bra a = Bra (Vector a)
-           | BTensor (Bra a) (Bra a)
+h = foldKet (\a b -> if b == 0 then (Sum 1 [Ket 1 0, Ket 1 1])
+                               else (Sum 1 [Ket 1 0, Ket (-1) 1]))
+            (\a b -> Tens a b)
+            (\a b -> Sum a b)
+x = foldKet (\a b -> if b == 0 then Ket a 1 else Ket a 0)
+            (\a b -> Tens a b)
+            (\a b -> Sum a b)
 
-instance Show (Ket a) where
-  show (Ket v) = "|" ++ (show v) ++ ">"
-
--- No idea what to do!
+z = foldKet (\a b -> if b == 0 then Ket a b else Ket (-a) b)
+            (\a b -> Tens a b)
+            (\a b -> Sum a b)
